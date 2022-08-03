35397483d0e068c05b10fa62fc6d219434481f27
yanguo <1304987399@qq.com>
2022-08-03 17:26:23 +0800

    '轮滑组件'

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,36 +1,138 @@
 <template>
-<div class="swiper">
-	<div style="background-image:url(../assets/2.jpeg)" data-swiper-parallax="-23%" data-swiper-parallax-duration="3000"></div>
-    <div class="swiper-wrapper">
-       <div class="swiper-slide">
-            <div class="title" data-swiper-parallax="-100">从右边100px开始进入</div>
-            <div class="subtitle" data-swiper-parallax="-200">从右边200px开始进入</div>
-            <div class="text" data-swiper-parallax="-300" data-swiper-parallax-duration="600">
-              <p>从右边300px开始进入，时间600ms</p>
-            </div>
-            <div data-swiper-parallax="0" data-swiper-parallax-opacity="0.5" >透明度变化</div>
-            <div data-swiper-parallax-scale="0.15" >缩放变化</div>
-        </div>
-    </div>
-</div>
+	<div id="swiper1" class="swiper">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide slide1"></div>
+			<div class="swiper-slide slide2"></div>
+			<div class="swiper-slide slide3"></div>
+		</div>
+		<!-- 如果需要分页器 -->
+		<div class="swiper-pagination"></div>
+	</div>
+	<div id="swiper2" class="swiper">
+		<div class="parallax-bg" data-swiper-parallax="-13%" data-swiper-parallax-duration="3000"></div>
+		<div class="swiper-wrapper">
+			<div class="swiper-slide slide1">
+				<div class="title" data-swiper-parallax="-200" data-swiper-parallax-opacity="0.5">1</div>
+			</div>
+			<div class="swiper-slide slide2">
+				<div class="title" data-swiper-parallax="-100">1</div>
+				<div class="title" data-swiper-parallax="-200">2</div>
+				<div class="title" data-swiper-parallax="-300">3</div>
+			</div>
+			<div class="swiper-slide slide3">
+				<div class="title" data-swiper-parallax="-200">3</div>
+			</div>
+		</div>
+		<!-- 如果需要分页器 -->
+		<div class="swiper-pagination"></div>
+	</div>
 </template>
 <script setup>
-	import { onMounted } from 'vue';
+	import { onMounted,ref } from 'vue';
+	const windowWidth = ref(0);
+	const windowHeight = ref(0);
+	window.onresize = () =>{
+		windowWidth.value =  window.innerWidth;
+		windowHeight.value =  window.innerHeight;
+	}
 	onMounted(() => {
-		var mySwiper = new Swiper ('.swiper', {
-			            parallax : true,
+		var mySwiper = new Swiper ('#swiper1', {
+			loop: true, // 循环模式选项
+			slidesPerView : 1,
+			slidesPerGroup : 1,
+			mousewheel: true,
+			// 如果需要分页器
+			pagination: {
+			  el: '.swiper-pagination',
+			},
 
+			// 如果需要前进后退按钮
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+
+			// 如果需要滚动条
+			scrollbar: {
+			  el: '.swiper-scrollbar',
+			},
+		}) 
+		var mySwiper = new Swiper ('#swiper2', {
+			slidesPerView : 1,
+			slidesPerGroup : 1,
+			mousewheel: true,
+			parallax:true,
+			speed: 1000,
+			// 如果需要分页器
+			pagination: {
+			  el: '.swiper-pagination',
+			},
+			
+			// 如果需要前进后退按钮
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+			
+			// 如果需要滚动条
+			scrollbar: {
+			  el: '.swiper-scrollbar',
+			},
 		}) 
 	})
 </script>
 <style scoped>
-	.swiper {
-	    width: 100%;
-	    height: 500px;
-	}  
-	.swiper-slide{
-		background-size: auto 100%;
-		background-repeat: no-repeat;
-		background-position: center;
+	@media screen and (max-width:800px){
+		#swiper1{
+			width: 100%;
+			height: 1080rem
+		}  
+		#swiper1 .swiper-slide{
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center;
+		}
+		#swiper1 .slide1{
+			background-image: url("../assets/2.jpeg");
+		}
+		#swiper1 .slide2{
+			background-image: url("../assets/3.jpeg");
+		}
+		#swiper1 .slide3{
+			background-image: url("../assets/2.jpeg");
+		}
+		#swiper2{
+			display: none;
+		}
+	}
+	@media screen and (min-width:800px){
+		#swiper2{
+			position: fixed;
+			width: 100%;
+			height: calc(100% - 55px);
+		}  
+		#swiper2 .parallax-bg{
+			position: fixed;
+			height: 100%;
+			width: 3000px;
+			background-repeat: no-repeat;
+			background-size: auto 100%;
+			background-image: url("../assets/2.jpeg");
+		}
+		#swiper2 .swiper-slide{
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center;
+			font-size: 100px;
+		}
+		#swiper2 .slide1{
+		}
+		#swiper2 .slide2{
+		}
+		#swiper2 .slide3{
+		}
+		#swiper1{
+			display: none;
+		}
 	}
 </style>
